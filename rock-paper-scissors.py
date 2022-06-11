275e449dc5e9a491c3c13378be7aeacabe4f0175
Afeez Akano <53193327+laplace125@users.noreply.github.com>
2022-06-11 23:15:52 +0100

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,27 +1,58 @@
 import random
+### To introduce the game
 print('This  is simple python version of the popular game \n"Rock Paper Scissors" \nTake Note that You are playing against the computer')
 rock_paper_scissors = ["Rock" , "Scissors" ,"Paper"]
-name = input('What is your name\n')
+name = input('What is your name\n=>')
 
+###### Function to take input from player , then convert the input to Rock , Paper or Scissors
+def take_input():
+    selection = input(f'Hello {name} \nEnter "R" for Rock , \n"P" for paper \n"S" for scissors\n')
+    if (selection == 'R' or selection == 'r'):
+        selection = "Rock"
+    elif (selection == 'P' or selection == 'p'):
+        selection= "Paper"
+    elif (selection == 'S' or selection == 's'):
+        selection = "Scissors"    
+    return selection
+
+####### Function to determine who wins or loses
+def win_or_lose():    
+    if selection == "Rock":
+        print(f'{name} wins Computer lose')    
+    elif (selection == "Scissors" and computer_guess== "Paper"):
+        print(f'{name} wins Computer lose')   
+    elif (selection== "Paper"):
+        print('Computer wins')
+    else: 
+        print('Computer wins')
+
+
+selection = take_input()
+
+### Check if selection is valid
 while True:
-    selection = int(input(f'Hello {name} , enter 1 for Rock , 2 for paper , 3 for scissors\n'))
-    if (selection > 0 and selection <4):
+    if(selection in rock_paper_scissors):
         break
-selection = rock_paper_scissors[selection -1]
+    selection = take_input()
 
-computer_guess = random.randrange(0 ,3)
-computer_guess = rock_paper_scissors[computer_guess]
+#### Print players selection and computer's random guess
+computer_guess = random.choice(["Rock" , "Scissors" ,"Paper"])
 print(f'{name} selected {selection} , while computer selected {computer_guess}')
-
-if selection == computer_guess:
+    
+        
+###Check if there is a tie
+while (selection == computer_guess):
     print('it is a tie')
-elif selection == "Rock":
-    print(f'{name} wins Computer lose')
-elif (selection == "Scissors" and computer_guess== "Paper"):
-    print(f'{name} wins Computer lose')
-elif (selection== "Paper"):
-    print('Computer wins')
-else: print('Computer wins')
+    computer_guess = random.choice(["Rock" , "Scissors" ,"Paper"])
+    selection = take_input()
+    print(f'{name} selected {selection} , while computer selected {computer_guess}')
+
+##### initialise the function win_or_lose()
+win_or_lose()
+      
+    
+
+        
 
 
 
